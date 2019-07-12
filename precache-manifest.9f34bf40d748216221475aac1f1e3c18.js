945b236e5416b6ca50f71238e990996634c5af0d
landroide13 <lazaro02@hotmail.com>
2019-07-12 20:54:24 +1200

    Updates

---
+++ precache-manifest.9f34bf40d748216221475aac1f1e3c18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04179d6dc7bda7deb0a03ae3de71114b",
+    "url": "/SimpleCurrencyApp/index.html"
+  },
+  {
+    "revision": "986e54fef63b6f1e0673",
+    "url": "/SimpleCurrencyApp/static/css/main.07dbf3cf.chunk.css"
+  },
+  {
+    "revision": "b3d76b3711d164e66d7a",
+    "url": "/SimpleCurrencyApp/static/js/2.44039830.chunk.js"
+  },
+  {
+    "revision": "986e54fef63b6f1e0673",
+    "url": "/SimpleCurrencyApp/static/js/main.300ef8a6.chunk.js"
+  },
+  {
+    "revision": "bfc5eae3a333f160ac20",
+    "url": "/SimpleCurrencyApp/static/js/runtime~main.56c6eb52.js"
+  }
+]);
